1122c76eef6521ceddbb970125228ffb57b355c6
kushagrasharma-13 <sharmakushagra295@gmail.com>
2024-07-04 22:39:09 +0530

    Dependecies resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: '/',
   plugins: [react()],
   preview: {
-    port: 8080,
+    port: 3000,
     strictPort: true,
+    host: '0.0.0.0', // Consistent with the server configuration
   },
   server: {
-    port: 8080,
+    port: 3000,
     strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:8080",
+    host: '0.0.0.0', // Listen on all IPs (useful for testing on mobile devices)
+    origin: 'http://localhost:3000', // The origin URL for the server
   },
 });
