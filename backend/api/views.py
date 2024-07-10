1be739ff49bf46b2be081c11b79fa155a9fafa0c
kushagrasharma-13 <sharmakushagra295@gmail.com>
2024-07-10 15:48:06 +0530

    URL

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,25 +12,25 @@ class healthdataViewSet(generics.ListAPIView):
 
 @api_view(['POST'])
 def health_recommendation(request):
-    # user_data = request.data
-    # medical_history = ''
-    # if 'name' in user_data:
-    #     try:
-    #         user = HealthData.objects.get(name=user_data['name'])
-    #         medical_history = user.medical_history
-    #     except HealthData.DoesNotExist:
-    #         user = HealthData(
-    #             name=user_data['name'],
-    #             age=user_data['age'],
-    #             gender=user_data['gender'],
-    #             medical_history=''
-    #         )
-    #         user.save()
-    # response = form_reponse(user_data['age'], user_data['gender'], user_data['symptoms'], medical_history)
-    # print(response)
-    # user.medical_history = concise_report(response)
-    # user.save()
-    with open(f'./api/example_output.json', 'r') as file:
-        response = json.load(file)
+    user_data = request.data
+    medical_history = ''
+    if 'name' in user_data:
+        try:
+            user = HealthData.objects.get(name=user_data['name'])
+            medical_history = user.medical_history
+        except HealthData.DoesNotExist:
+            user = HealthData(
+                name=user_data['name'],
+                age=user_data['age'],
+                gender=user_data['gender'],
+                medical_history=''
+            )
+            user.save()
+    response = form_reponse(user_data['age'], user_data['gender'], user_data['symptoms'], medical_history)
+    print(response)
+    user.medical_history = concise_report(response)
+    user.save()
+    # with open(f'./api/example_output.json', 'r') as file:
+    #     response = json.load(file)
     return Response(response)
 
